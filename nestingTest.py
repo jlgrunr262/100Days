dbe05d25ee3665ccb97491b03305cb442ec49846
Jerry Gentry <jlgrunr@gmail.com>
2021-07-27 20:46:34 -0400

    deleting a test file

--- nestingTest.py
+++
@@ -1,8 +0,0 @@
-travel_log = {
-    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-    "Germany": {"cities_visited": ["Berlin", "Frankfurt", "Freiberg"], "total_visits": 20},
-}
-
-print(travel_log)
-
-print(travel_log ["France"])
